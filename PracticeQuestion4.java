f16a42b00b84787f645de70d51bdd9d73e909707
Klye <158520080+KyleBX@users.noreply.github.com>
2025-07-24 20:15:32 -0400

    Update PracticeQuestion4.java

--- PracticeQuestion4.java
+++ PracticeQuestion4.java
@@ -14,7 +14,7 @@ public class PracticeQuestion4 {
         }
         int a = 0; 
         int b = 1; 
-     for (int i = 2; i <= n; i++) {
+        for (int i = 2; i <= n; i++) {
             int temp = a + b;
             a = b;
             b = temp;
