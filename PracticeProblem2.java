f9eea1ec871d88c64f20f6f9b228f043452adbed
Klye <158520080+KyleBX@users.noreply.github.com>
2025-07-24 20:12:36 -0400

    Update PracticeProblem2.java

--- PracticeProblem2.java
+++ PracticeProblem2.java
@@ -1,33 +1,17 @@
-public class PracticeProblem2 {
+public class PracticeQuestions1 {
     public static void main(String[] args) {
-      
-        int[] arr = {1, 2, 3, 4, 5};
+        int[] arr = {1, 3, 4, 4, 6, 7, 8};
+        int n = 4;
+        System.out.println("The number " + n + " appears " + countOccurrences(arr, n) + " times.");
+    }
 
-      
-        int[] reversedArr = reverseArray(arr);
-
-        
-        System.out.print("Reversed Array: [");
-        for (int i = 0; i < reversedArr.length; i++) {
-            System.out.print(reversedArr[i]);
-            if (i < reversedArr.length - 1) {
-                System.out.print(", ");
+    public static int countOccurrences(int[] arr, int n) {
+        int count = 0; 
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == n) { 
+                count++; 
             }
         }
-        System.out.println("]");  
+        return count; 
     }
-
-  
-    public static int[] reverseArray(int[] arr) {
-        int[] reversed = new int[arr.length]; 
-
-        for (int i = 0; i < arr.length; i++) {
-    
-            reversed[i] = arr[arr.length - 1 - i];
-        }
-
-        return reversed; 
-    }
-
 }
-
